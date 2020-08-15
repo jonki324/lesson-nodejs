d1ec86a7326e638299f298a7fdf164068251c1c9
jonki324 <jonki324@gmail.com>
2020-08-15 18:16:46 +0900

    Redis追加

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,28 @@
 const express = require('express')
+const redis = require('redis')
 const app = express()
 const port = 3000
 
+const client = redis.createClient();
+client.on('connect', () => {
+  console.log('Redis Connect')
+})
+client.on('error', err => {
+  console.error(`Redis Error: ${err}`)
+})
+
 app.get('/', (req, res) => {
+  client.set('hello', 'Hello Redis')
+  res.send('Hello World!')
+})
+
+app.get('/hello', (req, res) => {
+  client.get('hello', (err, result) => {
+    if (err) {
+      console.error(error)
+    }
+    console.log(result)
+  })
   res.send('Hello World!')
 })
 
