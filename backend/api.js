28053f5b51d82e1ec73bc1904d229b5bc617fa47
jonki324 <jonki324@gmail.com>
2020-08-23 04:12:30 +0900

    ステータスコード追加

--- backend/api.js
+++ backend/api.js
@@ -2,31 +2,47 @@ const { getConnection } = require('./db')
 const uuid = require('uuid')
 
 async function getUser(req, res) {
+  let statusCode = 500
+  const data = {user: null}
   const key = req.params.userId
   const client = await getConnection()
   await client.hgetallAsync(key).then(value => {
-    res.send({ msg: 'OK', user: value })
+    if (value === null) {
+      statusCode = 404
+    } else {
+      statusCode = 200
+      data.user = value
+    }
   }).catch(err => {
-    res.send({ msg: err, user: null })
-  }).finally(() => {
-    client.quit()
+    statusCode = 503
+    console.error(err)
+  }).finally(async () => {
+    await client.quit()
   })
+  res.status(statusCode).send(data)
 }
 
 async function getAllUser(req, res) {
+  let statusCode = 500
+  const data = {users: []}
   const client = await getConnection()
-  await client.keysAsync('*').then(keys => {
-    Promise.all(keys.map(key => client.hgetallAsync(key))).then(values => {
-      res.send({ msg: 'OK', users: values })
+  await client.keysAsync('*').then(async keys => {
+    await Promise.all(keys.map(key => client.hgetallAsync(key))).then(values => {
+      statusCode = 200
+      data.users = values
     })
   }).catch(err => {
-    res.send({ msg: err, users: [] })
-  }).finally(() => {
-    client.quit()
+    statusCode = 503
+    console.error(err)
+  }).finally(async () => {
+    await client.quit()
   })
+  res.status(statusCode).send(data)
 }
 
 async function addUser(req, res) {
+  let statusCode = 500
+  const data = {userId: null}
   const key = uuid.v4()
   const value = {
     id: key,
@@ -35,42 +51,65 @@ async function addUser(req, res) {
   }
   const client = await getConnection()
   await client.hmsetAsync(key, value).then(() => {
-    res.send({ msg: 'OK', userId: key })
+    statusCode = 201
+    data.userId = key
   }).catch(err => {
-    res.send({ msg: err, userId: ''})
-  }).finally(() => {
-    client.quit()
+    statusCode = 503
+    console.error(err)
+  }).finally(async () => {
+    await client.quit()
   })
+  res.status(statusCode).send(data)
 }
 
 async function modUser(req, res) {
+  let statusCode = 500
+  const data = {user: null}
   const key = req.params.userId
   const name = req.body.name
   const email = req.body.email
   const client = await getConnection()
   await client.hgetallAsync(key).then(async value => {
-    value.name = name
-    value.email = email
-    await client.hmsetAsync(key, value).then(() => {
-      res.send({ msg: 'OK', user: value })
-    })
+    if (value === null) {
+      statusCode = 404
+    } else {
+      value.name = name
+      value.email = email
+      await client.hmsetAsync(key, value).then(result => {
+        statusCode = 200
+        data.user = value
+      })
+    }
   }).catch(err => {
-    res.send({ msg: err, user: null })
-  }).finally(() => {
-    client.quit()
+    statusCode = 503
+    console.error(err)
+  }).finally(async () => {
+    await client.quit()
   })
+  res.status(statusCode).send(data)
 }
 
 async function rmvUser(req, res) {
+  let statusCode = 500
+  const data = {resultCount: 0}
   const key = req.params.userId
   const client = await getConnection()
-  await client.delAsync(key).then(result => {
-    res.send({ msg: 'OK', count: result })
+  await client.hgetallAsync(key).then(async value => {
+    if (value === null) {
+      statusCode = 404
+    } else {
+      await client.delAsync(key).then(result => {
+        statusCode = 200
+        data.resultCount = result
+      })
+    }
   }).catch(err => {
-    res.send({ msg: err, userId: key})
-  }).finally(() => {
-    client.quit()
+    statusCode = 503
+    console.error(err)
+  }).finally(async () => {
+    await client.quit()
   })
+  res.status(statusCode).send(data)
 }
 
 module.exports = {
